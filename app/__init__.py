3d638fcc8bfc83b6c41ee0c0f30c34c9cf268a64
Chuan Cheng <c_chuan23@hotmail.com>
2020-08-01 19:32:21 +0100

    Typo

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ corr_app = Flask(__name__)
 curr_env = os.environ.get('FLASK_ENV', None)
 
 if curr_env == 'production':
-    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL_DEV']
+    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 elif curr_env == 'development':
-    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
+    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL_DEV'] 
 corr_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(corr_app)
