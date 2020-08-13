f269c160e64743ab125ac70e74145218bf14faf3
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-08-13 16:55:39 +0100

    Fix typo

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,10 @@ from app.models import db
 corr_app = Flask(__name__)
 curr_env = os.environ.get('FLASK_ENV', None)
 
-if curr_env == 'production':
-    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL_DEV']
-elif curr_env == 'development':
-    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
+if curr_env == 'development':
+    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI_DEV']
+elif curr_env == 'production':
+    corr_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI'] 
 corr_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(corr_app)
