c187fe08ea5d7bb88806211397b964487be40181
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-08-17 10:54:28 +0100

    Add entrypoint for experiment purpose

--- correlation.py
+++ correlation.py
@@ -1,8 +1,14 @@
-from app import corr_app 
+from app import corr_app , db
+from app.models import CandleStick, Instrument
 
 @corr_app.route('/')
 def index():
     return 'Hello'
 
+@corr_app.route('/add_entry')
+def add_entry():
+    for i, c in db.session.query(Instrument, CandleStick).join(CandleStick, Instrument.id == CandleStick.ticker_id).all():
+        print(f"---{i} ---{c}")
+    return 'Done'
 if __name__ == '__main__':
     corr_app.run()
