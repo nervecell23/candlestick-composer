8feacb683724651506565f6be234ab9d046fecdd
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-08-17 10:46:28 +0100

    Add model for 'instrument' and modify schema for 'candlestick'

--- app/models.py
+++ app/models.py
@@ -2,11 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class CandleSticks(db.Model):
+class Instrument(db.Model):
+    __tablename__ = 'instruments'
     id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.DateTime)
-    ticker = db.Column(db.String(64))
+    ticker = db.Column(db.String(64), nullable=False, unique=True)
+
+class CandleStick(db.Model):
+    __tablename__ = 'candlesticks'
+    id = db.Column(db.Integer, primary_key=True)
     o = db.Column(db.Float)
     h = db.Column(db.Float)
     l = db.Column(db.Float)
     c = db.Column(db.Float)
+    ticker_id = db.Column(db.Integer, db.ForeignKey('instruments.id'), 
+            nullable=False)
+    instrument = db.relationship('Instrument', 
+            backref=db.backref('candlesticks', lazy=True))
