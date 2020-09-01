0b636ed0e46f639ff3889fd8eb6353221b761f8e
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-09-01 10:58:40 +0100

    Refactor 'Compare' and 'MultipleCompare' class so that business logic is reorganised

--- app/compare.py
+++ app/compare.py
@@ -2,12 +2,27 @@ import pickle
 import pandas as pd
 from app.candle_composer import CandleComposer
 from app.price_fetcher import PriceFetcher
-from app import oanda_api
 
 class Compare:
-    def __init__(self):
+    def __init__(self, api, ticker_a, ticker_b, granularity='D'):
+        self.price_fetcher = PriceFetcher(api)
         self.candle_composer = CandleComposer()
+        self.granularity = granularity 
+        self.ticker_a = ticker_a
+        self.ticker_b = ticker_b
 
+    def compare(self):
+        '''
+        Calculate Pearson correlation of two pandas series
+        '''
+        candle_list_a = self.price_fetcher.fetch_from_api(self.ticker_a)
+        candle_list_b = self.price_fetcher.fetch_from_api(self.ticker_b)
+        df_a = self.price_fetcher.convert_to_dataframe(candle_list_a)
+        df_b = self.price_fetcher.convert_to_dataframe(candle_list_b)
+        return_series_a = self.candle_composer.compose_return_series(df_a)
+        return_series_b = self.candle_composer.compose_return_series(df_b)
+        return return_series_a.corr(return_series_b)
+    
     def compare_from_file(self, loc_a, loc_b):
         '''
         Load pandas dataframe of candlesticks from pickle file
@@ -20,32 +35,23 @@ class Compare:
             return_series_b = self.candle_composer.compose_return_series(df, '8H')
         return return_series_a.corr(return_series_b)
         
-    def compare(self, df_return_a, df_return_b):
-        '''
-        Calculate Pearson correlation of two pandas series
-        '''
-        return df_return_a.corr(df_return_b)
-
 class MultipleCompare:
     def __init__(self):
-        self.price_fetcher = PriceFetcher(oanda_api)
-        self.comparer = Compare()
-        self.candle_composer = CandleComposer()
-        self.ticker_list = ['USD_CHF', 'USD_JPY', 'USD_CAD', 'USD_CHN',
-                            'USD_THB', 'USD_NOR', 'USD_ZAR', 'USD_CZK',
-                            'USD_SGD', 'USD_PLN', 'USD_MXN', 'USD_TRY']
+        # self.ticker_list = ['USD_CHF', 'USD_JPY', 'USD_CAD', 'USD_CHN',
+        #                     'USD_THB', 'USD_NOR', 'USD_ZAR', 'USD_CZK',
+        #                     'USD_SGD', 'USD_PLN', 'USD_MXN', 'USD_TRY']
+        self.ticker_list = ['USD_CHF', 'USD_JPY', 'USD_CHN']
+        self.length = len(self.ticker_list)
     
     def calculate_corr(self):
-        list_length = len(self.ticker_list)
-        for i in range(list_length):
-            for j in range(i+1, list_length):
+        result = [[None] * self.length for _ in range(self.length)]
+        for i in range(self.length):
+            for j in range(i+1, self.length):
                 ticker_a = self.ticker_list[i]
                 ticker_b = self.ticker_list[j]
-                df_a = self.price_fetcher.fetch_from_db(ticker_a)
-                df_b = self.price_fetcher.fetch_from_db(ticker_b)
-                df_return_a = self.candle_composer.compose_return_series(df_a, '8H')
-                df_return_b = self.candle_composer.compose_return_series(df_b, '8H') 
-                return self.comparer.compare(df_return_a, df_return_b)
+                compare_obj = Compare(ticker_a, ticker_b, granularity='D')
+                result[i][j] = compare_obj.compare()
+        return result
 
 
                 
