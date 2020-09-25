7dbc52e80f64a769365a8354ca91ad1ed32a2802
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-09-25 15:36:14 +0100

    Add methods description

--- app/price_fetcher.py
+++ app/price_fetcher.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime, timedelta
-from app.models import CandleSticks
+from app.models import Instrument
+
 
 class FetchPricesError(Exception):
     def __init__(self, status, body):
@@ -10,13 +11,24 @@ class FetchPricesError(Exception):
     def __str__(self):
         return f"status: {self.status}, msg: {self.body}"
 
+
 class PriceFetcher:
     def __init__(self, api):
-       self.api = api
+        self.api = api
 
-    def fetch_from_api(self, instrument):
+    def fetch_from_api(self, instrument, granularity='D', count=60):
+        """
+        Fetch prices for specified instrument
+
+        params:
+            count: Number of candlesticks to fetch
+
+        return:
+            List[Dict[DateTime, Float, Float, Float, Float]]
+        """
         kwargs = {}
-        kwargs['granularity'] = 'H1'
+        kwargs['granularity'] = granularity
+        kwargs['count'] = count
         response = self.api.instrument.candles(instrument, **kwargs)
         if response.status != 200:
             raise FetchPricesError(response.status, response.body)
@@ -28,27 +40,38 @@ class PriceFetcher:
             h = candle.mid.h
             l = candle.mid.l
             c = candle.mid.c
-            dt_candle.append({'DateTime': time, 'Open': o, 'High': h, 'Low': l, 'Close': c})
-        df = pd.DataFrame(dt_candle)
+            dt_candle.append({'DateTime': time, 'Open': o,
+                              'High': h, 'Low': l, 'Close': c})
+        return candle_list
+
+    def convert_to_dataframe(self, price_list):
+        """
+        Convert fetched price list into Pandas dataframe
+
+        params:
+            price_list: List of candlesticks fetched from OANDA API
+
+        return:
+            Pandas df
+        """
+        df = pd.DataFrame(price_list)
         df.set_index('DateTime', inplace=True)
         return df
 
     def fetch_from_db(self, instrument):
-        temp_list = []
-        candlesticks = CandleSticks.query.filter_by(ticker=instrument).candlesticks.limit(24*30).all()
-        candlesticks = CandleSticks.query.limit(24*30).all()
+        price_list = []
+        candlesticks = Instrument.query.filter_by(
+            ticker=instrument).candlesticks.limit(720).all()
         for candlestick in candlesticks:
             time = candlestick.datetime
             open = candlestick.o
             high = candlestick.h
             low = candlestick.l
             close = candlestick.c
-            temp_list.append({'Datetime': time, 'Open': open, 'High': high, 'Low': low, 'Close': close})
-        df = pd.DataFrame(temp_list)
-        df.set_index('DateTime', inplace=True)
-        return df
+            price_list.append({'Datetime': time, 'Open': open,
+                               'High': high, 'Low': low, 'Close': close})
+        return price_list
 
 
-        
 if __name__ == '__main__':
     pass
