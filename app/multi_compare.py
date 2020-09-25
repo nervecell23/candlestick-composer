e0f3880a8c7acd2236ca5668e8044181586e56d2
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-09-25 16:13:46 +0100

    Add method description

--- app/multi_compare.py
+++ app/multi_compare.py
@@ -7,11 +7,17 @@ class MultipleCompare:
         self.length = len(self.ticker_list)
 
     def calculate_corr(self):
+        """
+        Create a matrix of correlation
+
+        return:
+            2D array of correlation values
+        """
         result = [[None] * self.length for _ in range(self.length)]
         for i in range(self.length):
             for j in range(i+1, self.length):
-                ticker_a = self.ticker_list[i]
-                ticker_b = self.ticker_list[j]
-                compare_obj = Compare(ticker_a, ticker_b, granularity='D')
-                result[i][j] = compare_obj.compare()
+                instrument_a = self.ticker_list[i]
+                instrument_b = self.ticker_list[j]
+                compare_obj = Compare()
+                result[i][j] = compare_obj.compare(instrument_a, instrument_b)
         return result
