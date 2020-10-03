147c726103f35f248b91f772d035872facf9034a
Chuan Cheng <c_chuan23@hotmail.com>
2020-10-03 20:13:46 +0100

    Change how oanda api is configured from using ymal to using env vars

--- common/oanda_api_config.py
+++ common/oanda_api_config.py
@@ -3,6 +3,7 @@ import v20
 import yaml
 from pathlib import Path
 
+
 class ConfigPathError(Exception):
     def __init__(self, path):
         self.path = path
@@ -10,24 +11,29 @@ class ConfigPathError(Exception):
     def __str__(self):
         return f"{self.path} is invalid path"
 
+
 class Config:
 
     def __init__(self):
-          self.hostname = None
-          self.streaming_hostname = None
-          self.port = 443
-          self.ssl = True
-          self.token = None
-          self.username = None
-          self.accounts = []
-          self.active_account = None
-          self.path = None
-          self.datetime_format = "UNIX"
+        self.hostname = None
+        self.streaming_hostname = None
+        self.port = 443
+        self.ssl = True
+        self.token = None
+        self.username = None
+        self.accounts = []
+        self.active_account = None
+        self.path = None
+        self.datetime_format = "UNIX"
 
     def load(self):
-        if os.environ.get("FLASK_ENV", None) == "development":
-            path = Path(__file__).parent.parent / "secret" / "oanda_api_practise.yml"
-        elif os.environ.get("FLASK_ENV", None) == "production":
+        current_env = os.environ.get("FLASK_ENV", None)
+        print(f"** Load OANDA API config for {current_env} account")
+
+        if current_env == "development":
+            path = Path(__file__).parent.parent / \
+                "secret" / "oanda_api_practise.yml"
+        elif current_env == "production":
             path = Path(__file__).parent.parent / "secret" / "oanda_api.yml"
         try:
             with open(path) as f:
@@ -35,23 +41,26 @@ class Config:
                 self.username = y.get("username", self.username)
                 self.token = y.get("token", self.token)
                 self.accounts = y.get("accounts", self.accounts)
-                self.active_account = y.get("active_account", self.active_account)
+                self.active_account = y.get(
+                    "active_account", self.active_account)
                 self.hostname = y.get("hostname", self.hostname)
-                self.streaming_hostname = y.get("streaming_hostname", self.streaming_hostname)
+                self.streaming_hostname = y.get(
+                    "streaming_hostname", self.streaming_hostname)
                 self.port = y.get("port", self.port)
                 self.ssl = y.get("ssl", self.ssl)
-                self.datetime_format = y.get("datetime_format", self.datetime_format)
+                self.datetime_format = y.get(
+                    "datetime_format", self.datetime_format)
         except:
             raise ConfigPathError(path)
 
     def create_context(self):
         ctx = v20.Context(
-                self.hostname,
-                self.port,
-                self.ssl,
-                token=self.token,
-                datetime_format=self.datetime_format)
-        return ctx 
+            self.hostname,
+            self.port,
+            self.ssl,
+            token=self.token,
+            datetime_format=self.datetime_format)
+        return ctx
 
     def __str__(self):
         s = ""
@@ -67,8 +76,3 @@ class Config:
             s += "- {}\n".format(a)
         s += "active_account: {}".format(self.active_account)
         return s
-
-
-
-
-
